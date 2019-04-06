c607647f2161f8a3a4cb6eb81d0a78561c8a8646
tsushiy <28962399+tsushiy@users.noreply.github.com>
2019-04-06 17:17:40 +0900

    Changed popup behavior

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 Install: [Copy Tabs as Markdown - Chrome Web Store](https://chrome.google.com/webstore/detail/copy-tabs-as-markdown/bijpamokpnoogjbboljnepcbmookobnm)
 
 Copy current tab or all tabs URL and Title as Markdown easily.  
-You can also copy in any format as you like.
 
+Shortcuts:  
+Copy current tab: Alt+Shift+C  
+Copy all tabs: Alt+Shift+X  
 You can customize keyboards shortcuts.
+
+You can also copy in any format as you like.
 
 Multi-byte characters are not encoded in order to shorten URL.
 
 <img src="img\6bb5b07c13ae.png" alt="image.png" width=80%>
-
-<img src="img\8c1e4ce61b14.png" alt="image.png" width=80%>
 
 <img src="img\c0b3aa91f0c.png" alt="image.png" width=80%>
 
--- popup.js
+++ popup.js
@@ -1,11 +1,36 @@
-document.getElementById("currentTabCopy").onclick = function(){
-  let bg = chrome.extension.getBackgroundPage();
-  let command = 'copy-current-tab'
-  bg.copyInFormat(command)
-};
+function createTextbox() {
+  let textbox = document.createElement("textarea");
+  document.body.appendChild(textbox);
+  return textbox;
+}
 
-document.getElementById("allTabsCopy").onclick = function(){
-  let bg = chrome.extension.getBackgroundPage();
-  let command = 'copy-all-tabs'
-  bg.copyInFormat(command)
-};
+function executeCopy(textbox, text) {
+  textbox.value = text;
+  textbox.select();
+  document.execCommand('Copy');
+  document.body.removeChild(textbox);
+}
+
+function excapeUrl(url) {
+  url = decodeURI(url);
+  return url
+  .replace(/\(/g, escape)
+  .replace(/\)/g, escape)
+  .replace(/\[/g, escape)
+  .replace(/\]/g, escape)
+  .replace(/\ /g, escape);
+}
+
+window.onload = function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.storage.sync.get({
+      currentTabFormat: '[${title}](${url})',
+    }, function (options) {
+      let textbox = createTextbox()
+      let url = excapeUrl(tabs[0].url);
+      let title = tabs[0].title;
+      let text = options.currentTabFormat.replace('${title}', title).replace('${url}', url);
+      executeCopy(textbox, text);
+    });
+  });
+}
